29a7c0cc4cac652a921c815f92e1e67733a51545
Jiri Peterek <peterek@sourcelab.cz>
2020-04-01 20:10:14 +0200

    initialize main files

---
+++ config/development.config.js
@@ -0,0 +1,12 @@
+/**
+ * Common Development Config
+ *
+ * @param {Required<import("./index.js").HtmlWebpackPluginOptions>} options
+ * @returns {{ module: { rules : Array<any> }, plugins: Array<(new (): any)> }}
+ */
+exports = module.exports = options => ({
+    module: {
+        rules: []
+    },
+    plugins: []
+})
---
+++ index.js
@@ -0,0 +1,53 @@
+// @ts-check
+// webpack compiler options properties are marked as optional
+// although most of them are not optional
+// as a temporary work around this this is fixed with
+// `import('ts-essentials').DeepRequired`
+/** @typedef {import('ts-essentials').DeepRequired<import("webpack").Compiler>} WebpackCompiler */
+/**
+ * Plugin Options
+ * @typedef {{ mode?: 'production' | 'development'; }} HtmlWebpackPluginOptions
+ */
+const defaultOptions = {
+    mode: 'production',
+}
+
+class HtmlConfigWebpackPlugin
+{
+    /**
+     * @param {Partial<HtmlWebpackPluginOptions>} [options]
+     */
+    constructor (options = {})
+    {
+        this.options = Object.assign({}, defaultOptions, options)
+    }
+
+    /**
+     * @param {WebpackCompiler} compiler
+     */
+    apply (compiler)
+    {
+        const modeProduction = [
+            this.options.mode,
+            compiler.options.mode
+        ].some(mode => mode === 'production')
+
+        const options = {
+            mode: modeProduction ? 'production' : 'development',
+            ...this.options
+        }
+
+        const config = modeProduction
+            ? require('./config/production.config')(options)
+            : require('./config/development.config')(options)
+
+        // Merge config
+        compiler.options.plugins.push(...config.plugins)
+        compiler.hooks.afterEnvironment.tap('SassWebpackPlugin', () =>
+        {
+            compiler.options.module.rules.push(...config.module.rules)
+        })
+    }
+}
+
+exports = module.exports = HtmlConfigWebpackPlugin
